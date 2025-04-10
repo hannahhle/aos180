9e0791f40f72d863bfebbea57a66bda645a4c0d5
hannah <hannahle249@gmail.com>
2025-04-09 18:26:42 -0700

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,49 +1,64 @@
 import numpy as np
 import  matplotlib.pyplot as plt
 
-
-
-# organize and declare all variables
-
-# initialize all constants and parameters
+#constants and parameters
 
 g = 9.81 #m/s
 theta = 290 #K
 dtheta = 0.005 #K/m
 z_0 = 100 #m
 w_0 = 0 
-
-N = np.sqrt((g / theta) * dtheta)
-
-# loop : 1, nt 
-# t_total 
 T_tot = 2000 #seconds
+
 delta_t = 10
 
-nt = T_tot / delta_t
+N = np.sqrt((g / theta) * dtheta)
+nt = int(T_tot / delta_t)
 
+#figure out arrays
 time = np.linspace(0, T_tot, nt + 1)
-
-# figure out arrays and files
 zp = np.zeros(nt + 1)
 z_exact = np.zeros(nt + 1)
 w = np.zeros(nt + 1)
 errors = np.zeros(nt + 1)
 
+#initial conditions
 w[0] = w_0
 zp[0] = z_0
 z_exact[0] = z_0 * np.cos(N * time[0])
 errors[0] = abs(z_exact[0] - zp[0])
 
-for t in range(nt): #this is not right but i have my idea. ill think abt iterations later
-    t = t_0 + delta_t #current time
-# advance solution in time 
-    # each t (recursive eq)
-# update variables
-    t_0 = t
-# calculate exact soultion
-    z_exact
-    
-# calculate error 
-    err_t = 
+for t in range(nt): #an idea of sorts
+    zp[t + 1] = zp[t] + delta_t * w[t]  
+    w[t + 1] = w[t] + (delta_t * -N**2 * zp[t])
+    z_exact[t + 1] = z_0 * np.cos(N * time[t + 1])
+    errors[t + 1] = abs(z_exact[t + 1] - zp[t + 1])
 # save
+
+plt.figure()
+plt.plot(time, zp, label='numerical')
+plt.xlabel('time (s)')
+plt.ylabel('vertical position (m)')
+plt.legend()
+plt.title('position vs time (euler forward, delta t = 10)')
+plt.grid(True)
+plt.show()
+
+plt.figure()
+plt.plot(time, z_exact, '--', label='analytical')
+plt.xlabel('time (s)')
+plt.ylabel('vertical position (m)')
+plt.legend()
+plt.title('position vs time (euler forward, delta t = 10)')
+plt.grid(True)
+plt.show()
+
+plt.figure()
+plt.plot(time, errors)
+plt.xlabel('time (s)')
+plt.ylabel('error')
+#plt.xscale('log')
+plt.yscale('log')
+plt.title('euler forward error (delta t = 10)')
+plt.grid(True)
+plt.show()
