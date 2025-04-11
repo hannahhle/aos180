bfc7f771836e4cda3e27bb733e6702e63030f97d
hannah <hannahle249@gmail.com>
2025-04-11 01:10:39 -0700

    Update leapfroggy.py

--- hw1/leapfroggy.py
+++ hw1/leapfroggy.py
@@ -28,8 +28,11 @@ def leapfroggy(delta_t):
     z_exact[0] = z_0 * np.cos(N * time[0])
     errors[0] = abs(z_exact[0] - zp[0])
 
+    #euler-forward to start
     zp[1] = zp[0] + delta_t * w[0]
     w[1] = w[0] + (delta_t * -N**2 * zp[0])
+    z_exact[1] = z_0 * np.cos(N * time[1])
+    errors[1] = abs(z_exact[1] - zp[1])
 
     for t in range(1, nt): #an idea of sorts
         zp[t + 1] = zp[t - 1] + 2 * delta_t * w[t]  
@@ -39,17 +42,86 @@ def leapfroggy(delta_t):
         
     return time, zp, z_exact, w, errors
 
-time_10, zp_10, z_exact_10, w_10, errors_10 = leapfroggy(20)
+time_10, zp_10, z_exact_10, w_10, errors_10 = leapfroggy(10)
 time_1, zp_1, z_exact_1, w_1, errors_1 = leapfroggy(1)
 
 
+# FOR MY FIRST PART!!!
+
 plt.figure(1)
-plt.plot(time_10, zp_10, label='t = 20')
-plt.plot(time_1, zp_1, label='t = 1')
+plt.plot(time_10, zp_10, label='∆t = 10', color = 'slateblue')
+plt.plot(time_1, zp_1, label='∆t = 1', color = 'indigo')
 plt.xlabel('time (s)')
 plt.ylabel('vertical position (m)')
 plt.legend()
-plt.title('position vs time (leapfrog, delta t =')
+plt.title('position vs time (leapfrog)')
 plt.grid(True)
 plt.show()
-# save
+
+plt.figure(2)
+plt.plot(time_10, zp_10, label='numerical', color = 'slateblue')
+plt.plot(time_10, z_exact_10, label='exact', color = 'firebrick')
+plt.xlabel('time (s)')
+plt.ylabel('vertical position (m)')
+plt.legend()
+plt.title('position vs time (leapfrog ∆t = 10)')
+plt.grid(True)
+plt.show()
+
+plt.figure(3)
+plt.plot(time_1, zp_1, label='numerical', color = 'indigo')
+plt.plot(time_1, z_exact_1, label='exact', color = 'firebrick')
+plt.xlabel('time (s)')
+plt.ylabel('vertical position (m)')
+plt.legend()
+plt.title('position vs time (leapfrog ∆t = 1)')
+plt.grid(True)
+plt.show()
+
+# PART NUMBA TWOOOOOO
+
+#everything everywhere all at once
+plt.figure(4)
+plt.plot(zp_10, w_10, label='∆t = 10', color = 'slateblue')
+plt.plot(zp_1, w_1, label = '∆t = 1', color = 'indigo')
+plt.xlabel('vertical position (m)')
+plt.ylabel('vertical velocity (m/s)')
+plt.legend()
+plt.title('position (z) vs velocity (w) [leapfrog]')
+plt.grid(True)
+plt.show()
+
+#just t=10
+plt.figure(5)
+plt.plot(zp_10, w_10, label='∆t = 10', color = 'slateblue')
+plt.xlabel('vertical position (m)')
+plt.ylabel('vertical velocity (m/s)')
+plt.legend()
+plt.title('position (z) vs velocity (w) [leapfrog ∆t = 10]')
+plt.grid(True)
+plt.show()
+
+#just t=1
+plt.figure(6)
+plt.plot(zp_1, w_1, label = '∆t = 1', color = 'indigo')
+plt.xlabel('vertical position (m)')
+plt.ylabel('vertical velocity (m/s)')
+plt.legend()
+plt.title('position (z) vs velocity (w) [leapfrog ∆t = 1]')
+plt.grid(True)
+plt.show()
+
+#PTHREE
+
+# my errors (sadddd)
+plt.figure(3)
+plt.plot(time_10, errors_10, label = '∆t = 10', color = 'slateblue')
+plt.plot(time_1, errors_1, label = '∆t = 1', color = 'indigo')
+plt.legend()
+plt.xlabel('time (s)')
+plt.ylabel('error')
+plt.xscale('log')
+plt.yscale('log')
+plt.title('leapfrog error')
+plt.grid(True)
+plt.show()
